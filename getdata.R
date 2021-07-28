e83c590c67af1366049a51b3631c98e0a4151ebc
Ivo Ruaro <ruaro.ivo@gmail.com>
2021-07-28 10:20:14 +0200

    Test

---
+++ getdata.R
@@ -0,0 +1,18 @@
+library(tidyverse)
+library(lubridate)
+# 
+# 
+# somministrazione_vaccini <-
+#     read_csv('https://raw.githubusercontent.com/italia/covid19-opendata-vaccini/master/dati/somministrazioni-vaccini-summary-latest.csv') %>%
+#     mutate(data_somministrazione = ymd(data_somministrazione))
+# 
+# df <- somministrazione_vaccini %>%
+#     filter(data_somministrazione > (max(data_somministrazione) - 90)) %>%
+#     group_by(data_somministrazione) %>%
+#     summarize(
+#         prima_dose = sum(prima_dose),
+#         ciclo_completo = sum(seconda_dose) + sum(pregressa_infezione),
+#         
+#     )
+# 
+# write_csv(df, 'csv/somministrazione_vaccini_90_giorni.csv')
